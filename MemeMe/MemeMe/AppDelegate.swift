29bf483d7807ac341204cf5c304ebf23cccfc9e6
wagnersouz4 <wagnersouz4@gmail.com>
2017-03-08 23:40:06 -0300

    adding layout and basic funcionality

--- MemeMe/MemeMe/AppDelegate.swift
+++ MemeMe/MemeMe/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  MemeMe
 //
-//  Created by Pan on 8/03/17.
+//  Created by Wagner Souza on 8/03/17.
 //  Copyright © 2017 Wagner Souza. All rights reserved.
 //
 
--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -2,12 +2,37 @@
 //  ViewController.swift
 //  MemeMe
 //
-//  Created by Pan on 8/03/17.
+//  Created by Wagner Souza on 8/03/17.
 //  Copyright © 2017 Wagner Souza. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    // UIImageView containing the selected image
+    @IBOutlet weak var selectedImage: UIImageView!
 
+    @IBOutlet weak var selectFromCameraButton: UIBarButtonItem!
+    @IBOutlet weak var selectFromAlbumButton: UIBarButtonItem!
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Enable the select from camera feature only if there is a camera available.
+        selectFromCameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+
+    @IBAction func selectImageFromCamera() {
+        presentUIImagePickerWithSourceType(.camera)
+    }
+
+    @IBAction func selectImageFromAlbum() {
+        presentUIImagePickerWithSourceType(.savedPhotosAlbum)
+    }
+
+    // Preseting a UIImagePickerController with a specific sourceType
+    private func presentUIImagePickerWithSourceType(_ sourceType: UIImagePickerControllerSourceType) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        present(imagePicker, animated: true)
+    }
 }
