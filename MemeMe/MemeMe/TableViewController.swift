9079ba3f7eb64b4505acc2c8b8c949ec2407cefa
wagnersouz4 <wagnersouz4@gmail.com>
2017-03-11 01:56:14 -0300

    loading the stored memes array into table and collection view controllers

---
+++ MemeMe/MemeMe/TableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewController.swift
+//  MemeMe
+//
+//  Created by Wagner Souza on 11/03/17.
+//  Copyright © 2017 Wagner Souza. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+    private var memes = [Meme]()
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Loading the memes
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            memes = appDelegate.memes
+        }
+    }
+}
